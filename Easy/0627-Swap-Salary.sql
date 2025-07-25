321656cf2b213a8acecd985b5a973b0c7297d2d2
Sawarnshivam <sawarnshivam9895@gmail.com>
2025-07-25 22:27:52 +0530

    Added problem description and solution for 627 Swap Salary

--- Easy/0627-Swap-Salary.sql
+++ Easy/0627-Swap-Salary.sql
@@ -1,8 +1,34 @@
 -- LeetCode 627: Swap Salary
 -- https://leetcode.com/problems/swap-salary/
 
+-- LeetCode 627: Swap Salary
+-- Difficulty: Easy
+-- https://leetcode.com/problems/swap-salary/
+
+-- 📝 Problem Description:
+-- Table: Salary
+--
+-- +-------------+----------+
+-- | Column Name | Type     |
+-- +-------------+----------+
+-- | id          | int      |
+-- | name        | varchar  |
+-- | sex         | ENUM     |
+-- | salary      | int      |
+-- +-------------+----------+
+-- id is the primary key for this table.
+-- The sex column is ENUM value of type ('m', 'f').
+-- The table contains information about an employee.
+--
+-- Write a SQL query to swap all 'f' and 'm' values in the sex column.
+-- Use a single UPDATE statement and no intermediate SELECT or temp tables.
+
+-- ✅ Solution:
 UPDATE Salary
 SET sex = CASE
             WHEN sex = 'm' THEN 'f'
             ELSE 'm'
           END;
+
+
+
